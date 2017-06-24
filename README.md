af1ab1a0246cb03f8c777c646ae871c5a7df00d6
Jason H <jahenry@gwmail.gwu.edu>
2017-06-24 12:45:43 -0400

    First file upload

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# MomsConnectPrj1
-Team 4 Moms connection project
+# Team4Project1
+First Team 4 Project
+
+# Firebase Connection Information between <script> tags here
+<script src="https://www.gstatic.com/firebasejs/4.1.3/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDrLO-RSZ-B6BD4gxJXqCOnMLA19DFHcsI",
+    authDomain: "momsconnection-63998.firebaseapp.com",
+    databaseURL: "https://momsconnection-63998.firebaseio.com",
+    projectId: "momsconnection-63998",
+    storageBucket: "",
+    messagingSenderId: "102438011730"
+  };
+  firebase.initializeApp(config);
+</script>
---
+++ Registration.js
@@ -0,0 +1,41 @@
+//Registration method
+function fnRegisterMom(){
+	var config = {
+	    apiKey: "AIzaSyA2luHS-fHr1w65HpiL7HWS2NO8-r-pFAQ",
+	    authDomain: "team4empdataproject.firebaseapp.com",
+	    databaseURL: "https://team4empdataproject.firebaseio.com",
+	    projectId: "team4empdataproject",
+	    storageBucket: "",
+	    messagingSenderId: "863126920899"
+	  };
+
+    firebase.initializeApp(config);
+
+    // Create a variable to reference the database
+    var database = firebase.database();
+    // Capture Button Click
+    event.preventDefault();
+    
+    var name = $("#name-input").val().trim();
+    var address1 = $("#address1-input").val().trim();
+    var address2 = $("#address2-input").val().trim();
+    var city = $("#city-input").val().trim();
+    var state = $("#state-input").val().trim();
+    var zipcode = $("#zipcode-input").val().trim();
+    var email = $("#email-input").val().trim();
+    var phone = $("#phone-input").val().trim();
+    var password = $("#password-input").val().trim();
+    var regdate = moment($("#regdate-input").val().trim(), "MM/DD/YY").format("X");
+    var childage = $("#childage-input").val().trim();
+    var boygirl = $("#boygirl-input").val().trim();
+
+
+      database.ref().push({
+        name: name,
+        role: role,
+        startdate: startdate,
+        monthrate: monthrate
+      });
+}
+
+//Pull Registration data
