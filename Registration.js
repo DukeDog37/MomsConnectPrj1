12414aa4901b3ddbfbda0d9d058fcc96a2b341eb
Jason H <jahenry@gwmail.gwu.edu>
2017-06-25 14:47:09 -0400

    Version with woring modal registration form

--- Registration.js
+++ Registration.js
@@ -1,13 +1,16 @@
+
+
 //Registration method
 function fnRegisterMom(){
-	var config = {
-	    apiKey: "AIzaSyA2luHS-fHr1w65HpiL7HWS2NO8-r-pFAQ",
-	    authDomain: "team4empdataproject.firebaseapp.com",
-	    databaseURL: "https://team4empdataproject.firebaseio.com",
-	    projectId: "team4empdataproject",
-	    storageBucket: "",
-	    messagingSenderId: "863126920899"
-	  };
+	alert("in registration");
+    var config = {
+    apiKey: "AIzaSyDrLO-RSZ-B6BD4gxJXqCOnMLA19DFHcsI",
+    authDomain: "momsconnection-63998.firebaseapp.com",
+    databaseURL: "https://momsconnection-63998.firebaseio.com",
+    projectId: "momsconnection-63998",
+    storageBucket: "momsconnection-63998.appspot.com",
+    messagingSenderId: "102438011730"
+  };
 
     firebase.initializeApp(config);
 
@@ -23,14 +26,28 @@ function fnRegisterMom(){
     var state = $("#state-input").val().trim();
     var zipcode = $("#zipcode-input").val().trim();
     var email = $("#email-input").val().trim();
+    var userId = email.substring(0, email.indexOf("@"));
     var phone = $("#phone-input").val().trim();
     var password = $("#password-input").val().trim();
-    var regdate = moment($("#regdate-input").val().trim(), "MM/DD/YY").format("X");
+    var regdate = new Date();
     var childage = $("#childage-input").val().trim();
-    var boygirl = $("#boygirl-input").val().trim();
-
-
-      database.ref().push({
+    if($("#chkboy").is(":checked")){
+        var boygirl = "boy";
+    }
+    else if($("#chkgirl").is(":checked")){
+        var boygirl = "girl";
+    }
+    else{
+        var boygirl = "Not Specified";
+    }
+    if($("#chkRemember").is(":checked")){
+        var remUser = "yes";
+    }
+    else{
+        var remUser = "no";
+    }
+    
+      database.ref('moms/' + userId).set({
         name: name,
         address1: address1,
         address2: address2,
@@ -42,8 +59,12 @@ function fnRegisterMom(){
         password: password,
         regdate: regdate,
         childage: childage,
-        boygirl: boygirl
+        boygirl: boygirl,
+        rememberUser: remUser
       });
+
+  
+
 }
 
 //Pull Registration data
